53ff1c39a98946502b9e3571fc8a4d9a58ed21dd
Dheeraj Kumar <dheeraj.kumar@salesforce.com>
2021-02-21 02:38:56 +0530

    first commit

---
+++ src/test/java/com/dheeraj/likesService/LikesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dheeraj.likesService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
